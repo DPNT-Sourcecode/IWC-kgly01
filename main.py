db47c1fa1e15558795031f0d0039be13971b1c2d
sbx_user1051 <sbx_user1051@169.254.30.185>
2026-01-17 18:12:19 +0000

    Sat Jan 17 18:12:19 UTC 2026

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+from test.solution_tests.IWC.test_queue_solution import test_enqueue_size_dequeue_flow
+
+
 def main():
     print("Hello from accelerate-runner!")
 
 
 if __name__ == "__main__":
-    main()
+    test_enqueue_size_dequeue_flow()
+
