e94241cfa42d73a10526eb422b56e1f2f46f92a6
sbx_user1051 <sbx_user1051@169.254.30.185>
2026-01-17 15:28:55 +0000

    Sat Jan 17 15:28:55 UTC 2026

--- test/solution_tests/IWC/test_queue_solution.py
+++ test/solution_tests/IWC/test_queue_solution.py
@@ -19,18 +19,45 @@ def test_enqueue_size_dequeue_flow() -> None:
         ]
     )
 
+    # def test_enqueue_size_dequeue_flow() -> None:
 
-# def test_enqueue_size_dequeue_flow() -> None:
+    entry_1 = {
+        "user_id": 1,
+        "provider": "companies_house",
+        "timestamp": iso_ts(delta_minutes=0),
+    }
+    entry_2 = {
+        "user_id": 2,
+        "provider": "bank_statements",
+        "timestamp": iso_ts(delta_minutes=0),
+    }
+    entry_3 = {
+        "user_id": 1,
+        "provider": "id_verification",
+        "timestamp": iso_ts(delta_minutes=0),
+    }
+    entry_4 = {
+        "user_id": 1,
+        "provider": "bank_statements",
+        "timestamp": iso_ts(delta_minutes=0),
+    }
 
-#     entry_1 = {"user_id": 1, "provider":"companies_house", "timestamp":iso_ts(delta_minutes=0)}
-#     entry_2 = {"user_id": 2, "provider":"bank_statements", "timestamp":iso_ts(delta_minutes=0)}
-#     entry_3 = {"user_id": 1, "provider":"id_verification", "timestamp":iso_ts(delta_minutes=0)}
-#     entry_4 = {"user_id": 1, "provider":"bank_statements", "timestamp":iso_ts(delta_minutes=0)}
+    run_queue(
+        [
+            call_enqueue(
+                entry_1["provider"], entry_1["user_id"], entry_1["timestamp"]
+            ).expect(1),
+            call_enqueue(
+                entry_2["provider"], entry_2["user_id"], entry_2["timestamp"]
+            ).expect(2),
+            call_enqueue(
+                entry_3["provider"], entry_3["user_id"], entry_3["timestamp"]
+            ).expect(3),
+            call_enqueue(
+                entry_4["provider"], entry_4["user_id"], entry_4["timestamp"]
+            ).expect(4),
+            call_size().expect(4),
+            # call_dequeue().expect("companies_house", 1),
+        ]
+    )
 
-#     run_queue(
-#         [
-#             call_enqueue("companies_house", 1, iso_ts(delta_minutes=0)).expect(1),
-#             call_size().expect(1),
-#             call_dequeue().expect("companies_house", 1),
-#         ]
-#     )
