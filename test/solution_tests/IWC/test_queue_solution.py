07143c1aa45f4a16dacc9f9f899aae07cc399bb2
sbx_user1051 <sbx_user1051@169.254.30.185>
2026-01-17 15:49:55 +0000

    Sat Jan 17 15:49:55 UTC 2026

--- test/solution_tests/IWC/test_queue_solution.py
+++ test/solution_tests/IWC/test_queue_solution.py
@@ -2,23 +2,6 @@ from __future__ import annotations
 
 from .utils import call_dequeue, call_enqueue, call_size, iso_ts, run_queue
 from typing import Any
-import pytest
-
-
-# @pytest.mark.parametrize(
-#     "user_id",
-#     "provider",
-#     "timestamp",
-#     [(1, "companies_house", iso_ts(delta_minutes=0))],
-# )
-def test_enqueue_size_dequeue_flow() -> None:
-    run_queue(
-        [
-            call_enqueue("companies_house", 1, iso_ts(delta_minutes=0)).expect(1),
-            call_size().expect(1),
-            call_dequeue().expect("companies_house", 1),
-        ]
-    )
 
 
 def get_test_data() -> dict[str, dict[str, Any]]:
@@ -43,14 +26,31 @@ def get_test_data() -> dict[str, dict[str, Any]]:
         "timestamp": iso_ts(delta_minutes=0),
     }
 
+    entry_5 = {
+        "user_id": 1,
+        "provider": "credit_check",
+        "timestamp": iso_ts(delta_minutes=0),
+    }
+
     test_data = {
         "entry_1": entry_1,
         "entry_2": entry_2,
         "entry_3": entry_3,
         "entry_4": entry_4,
+        "entry_5": entry_5,
     }
 
     return test_data
+
+
+def test_enqueue_size_dequeue_flow() -> None:
+    run_queue(
+        [
+            call_enqueue("companies_house", 1, iso_ts(delta_minutes=0)).expect(1),
+            call_size().expect(1),
+            call_dequeue().expect("companies_house", 1),
+        ]
+    )
 
 
 def test_prioritisations_rule_of_3() -> None:
@@ -110,27 +110,24 @@ def test_prioritisations_timestamp_ordering() -> None:
         ]
     )
 
-    def test_prioritisations_dependecny_resolution() -> None:
+
+def test_prioritisations_dependency_resolution() -> None:
     test_data = get_test_data()
 
     run_queue(
         [
             call_enqueue(
-                test_data["entry_4"]["provider"],
-                test_data["entry_4"]["user_id"],
-                iso_ts(delta_minutes=5),
-            ).expect(1),
-            call_enqueue(
-                test_data["entry_2"]["provider"],
-                test_data["entry_2"]["user_id"],
-                test_data["entry_2"]["timestamp"],
+                test_data["entry_5"]["provider"],
+                test_data["entry_5"]["user_id"],
+                test_data["entry_5"]["timestamp"],
             ).expect(2),
             call_size().expect(2),
-            call_dequeue().expect("bank_statements", 2),
-            call_dequeue().expect("bank_statements", 1),
+            call_dequeue().expect("companies_house", 1),
+            call_dequeue().expect("credit_check", 1),
             call_size().expect(0),
         ]
     )
+
 
 
 
